b6bc08a812954d7c5551ff8784f1fbb56a30322b
MathDotSqrt <ctrenkov@gmail.com>
2018-12-23 17:32:12 -0500

    Implemented basic 2d infrastructure. Have not implemented planet curvature

---
+++ Assets/Code/Character2DController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2DController : MonoBehaviour {
+	public float jumpForce = 100f;
+	public float moveSpeed = 10f;
+	[Range(0f, 1f)] public float smoothedTime = .5f;
+	[Range(0f, 1f)] public float crouchSpeed = .3f;
+
+	public LayerMask whatIsGround;
+
+	public Transform groundCheck = null;
+	public Transform ceilingCheck = null;
+
+	public float groundRadius = .1f;
+	public float ceilingRadius = .1f;
+
+
+	private Transform playerTransform;
+	private Rigidbody2D playerRigidBody;
+	private Animator playerAnimator;
+
+
+
+	private bool isOnGround = false;
+	private float playerDirection = 1;
+	private Vector2 smoothedVel = new Vector2();
+
+	void Start() {
+		playerTransform = GetComponent<Transform>();
+		playerRigidBody = GetComponent<Rigidbody2D>();
+		playerAnimator = GetComponent<Animator>();
+
+	}
+
+	void FixedUpdate() {
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundRadius, whatIsGround);
+
+		isOnGround = false;
+
+		foreach (Collider2D c in colliders) {
+			if (c.gameObject != base.gameObject) {
+				isOnGround = true;
+				break;
+			}
+		}
+
+		playerAnimator.SetFloat("Speed", Mathf.Abs(playerRigidBody.velocity.x));
+		playerAnimator.SetFloat("VerticalVelocity", playerRigidBody.velocity.y);
+		playerAnimator.SetBool("IsOnGround", isOnGround);
+
+	}
+
+	public void Move(float direction) {
+		if (direction != 0 && Mathf.Sign(direction) != Mathf.Sign(playerDirection))
+			Flip();
+
+		Vector2 currentVel = playerRigidBody.velocity;
+		Vector2 targetVel = new Vector2(direction * moveSpeed, currentVel.y);
+
+		playerRigidBody.velocity = Vector2.SmoothDamp(currentVel, targetVel, ref smoothedVel, smoothedTime);
+	}
+
+	public void Jump() {
+		if (isOnGround) {
+			isOnGround = false;
+			Vector2 currentVel = playerRigidBody.velocity;
+			currentVel.y = jumpForce;
+			playerRigidBody.velocity = currentVel;
+		}
+	}
+
+	public void Flip() {
+		playerDirection = -playerDirection;
+		playerTransform.Rotate(0, playerDirection * 180 + playerTransform.rotation.y, 0);
+	}
+}
---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Character2DController controller;
+
+	private float horizontal = 0;
+	private bool jump = false;
+
+	void Start() {
+		controller = GetComponent<Character2DController>();
+	}
+
+	void Update() {
+		horizontal = Input.GetAxisRaw("Horizontal");
+		jump = Input.GetKey("space");
+	}
+
+	void FixedUpdate() {
+		controller.Move(horizontal * Time.fixedDeltaTime);
+		if (jump) {
+			controller.Jump();
+		}
+	}
+}
